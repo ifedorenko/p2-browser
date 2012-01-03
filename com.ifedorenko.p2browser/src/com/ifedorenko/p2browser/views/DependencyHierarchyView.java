3c681d1ccf19baaf9370ff4341bc80013712f5f2
Igor Fedorenko <igor@ifedorenko.com>
2012-01-03 11:42:45 -0500

    Dependency hierarchy filtering and sorting
    Shamelessly re-implemented m2e dependency hierarchy filtering and
    sorting.
    
    Signed-off-by: Igor Fedorenko <igor@ifedorenko.com>

--- com.ifedorenko.p2browser/src/com/ifedorenko/p2browser/views/DependencyHierarchyView.java
+++ com.ifedorenko.p2browser/src/com/ifedorenko/p2browser/views/DependencyHierarchyView.java
@@ -14,8 +14,11 @@ package com.ifedorenko.p2browser.views;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.EventObject;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -30,9 +33,12 @@ import org.eclipse.equinox.p2.metadata.MetadataFactory.InstallableUnitDescriptio
 import org.eclipse.equinox.p2.metadata.Version;
 import org.eclipse.equinox.p2.metadata.VersionRange;
 import org.eclipse.equinox.p2.query.IQueryable;
-import org.eclipse.jface.action.IMenuManager;
-import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.Separator;
+import org.eclipse.jface.action.ToolBarManager;
+import org.eclipse.jface.viewers.IFontProvider;
 import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.IStructuredContentProvider;
 import org.eclipse.jface.viewers.IStructuredSelection;
@@ -40,20 +46,27 @@ import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerFilter;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.SashForm;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.FontData;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.ToolBar;
 import org.eclipse.swt.widgets.Tree;
+import org.eclipse.ui.forms.widgets.Section;
 import org.eclipse.ui.part.ViewPart;
+import org.eclipse.wb.swt.ResourceManager;
 
 import com.ifedorenko.p2browser.director.InstallableUnitDAG;
 import com.ifedorenko.p2browser.model.IGroupedInstallableUnits;
 import com.ifedorenko.p2browser.model.InstallableUnitDependencyTree;
+import com.ifedorenko.p2browser.model.match.IInstallableUnitMatcher;
 import com.ifedorenko.p2browser.model.match.InstallableUnitsMatcher;
+import com.ifedorenko.p2browser.views.InstallableUnitFilterComposite.IFilterChangeListener;
 
 import copied.org.eclipse.equinox.internal.p2.director.PermissiveSlicer;
 
@@ -64,11 +77,43 @@ public class DependencyHierarchyView
 
     public static final String ID = "com.ifedorenko.p2browser.views.DependencyHierarchyView"; //$NON-NLS-1$
 
-    private TreeViewer hierarchyTreeViewer;
+    TreeViewer hierarchyTreeViewer;
 
-    private TableViewer listTableViewer;
+    TableViewer listTableViewer;
 
     InstallableUnitDAG dag;
+
+    IInstallableUnitMatcher unitMatcher;
+
+    Font boldFont;
+
+    boolean filterHierarchy = true;
+
+    boolean filterList = false;
+
+    boolean sortList = true;
+
+    private Action filterHierarchyAction;
+
+    // TODO share decoration logic with MetadataRepositoryView
+    private class LabelProvider
+        extends InstallableUnitLabelProvider
+        implements IFontProvider
+    {
+        @Override
+        public Font getFont( Object element )
+        {
+            if ( unitMatcher != null )
+            {
+                IInstallableUnit unit = toInstallableUnit( element );
+                if ( unit != null && unitMatcher.match( unit ) )
+                {
+                    return boldFont;
+                }
+            }
+            return null;
+        }
+    };
 
     public DependencyHierarchyView()
     {
@@ -77,19 +122,82 @@ public class DependencyHierarchyView
     @Override
     public void createPartControl( Composite parent )
     {
-        SashForm sashForm = new SashForm( parent, SWT.NONE );
+        Composite composite = new Composite( parent, SWT.NONE );
+        GridLayout gl_composite = new GridLayout( 1, false );
+        gl_composite.marginWidth = 0;
+        gl_composite.marginHeight = 0;
+        composite.setLayout( gl_composite );
 
-        Composite hierarchyComposite = new Composite( sashForm, SWT.NONE );
-        hierarchyComposite.setLayout( new GridLayout( 1, false ) );
+        final InstallableUnitFilterComposite filterComposite = new InstallableUnitFilterComposite( composite, SWT.NONE );
+        filterComposite.addFilterChangeListener( new IFilterChangeListener()
+        {
+            public void filterChanged( EventObject event )
+            {
+                unitMatcher = filterComposite.getMatcher();
+                applyHierachyFilter();
+                applyListFilter();
+            }
+        } );
+        filterComposite.setLayoutData( new GridData( SWT.FILL, SWT.CENTER, true, false, 1, 1 ) );
 
-        Label lblHierarchy = new Label( hierarchyComposite, SWT.NONE );
-        lblHierarchy.setText( "Dependency Hierarchy" );
+        SashForm sashForm = new SashForm( composite, SWT.NONE );
+        sashForm.setLayoutData( new GridData( SWT.FILL, SWT.FILL, true, true, 1, 1 ) );
 
-        hierarchyTreeViewer = new TreeViewer( hierarchyComposite, SWT.BORDER | SWT.VIRTUAL );
+        Section hierarchySection = new Section( sashForm, Section.TITLE_BAR );
+        hierarchySection.setText( "Dependency Hierarchy" );
+
+        ToolBar hierarchyToolBar = new ToolBar( hierarchySection, SWT.FLAT | SWT.RIGHT );
+        hierarchySection.setTextClient( hierarchyToolBar );
+
+        ToolBarManager hierarchyToolBarManager = new ToolBarManager( hierarchyToolBar );
+        hierarchyToolBarManager.add( new Action( "Expand All",
+                                                 ResourceManager.getPluginImageDescriptor( "com.ifedorenko.p2browser",
+                                                                                           "icons/expandall.gif" ) )
+        {
+            public void run()
+            {
+                hierarchyTreeViewer.getTree().setRedraw( false );
+                hierarchyTreeViewer.expandAll();
+                hierarchyTreeViewer.getTree().setRedraw( true );
+            };
+        } );
+        hierarchyToolBarManager.add( new Action( "Collapse All",
+                                                 ResourceManager.getPluginImageDescriptor( "com.ifedorenko.p2browser",
+                                                                                           "icons/collapseall.gif" ) )
+        {
+            public void run()
+            {
+                hierarchyTreeViewer.getTree().setRedraw( false );
+                hierarchyTreeViewer.collapseAll();
+                hierarchyTreeViewer.expandToLevel( 2 );
+                hierarchyTreeViewer.getTree().setRedraw( true );
+            };
+        } );
+        hierarchyToolBarManager.add( new Separator() );
+        filterHierarchyAction =
+            new Action( "Filter", ResourceManager.getPluginImageDescriptor( "com.ifedorenko.p2browser",
+                                                                            "icons/filter.gif" ) )
+            {
+                @Override
+                public int getStyle()
+                {
+                    return AS_CHECK_BOX;
+                }
+
+                public void run()
+                {
+                    filterHierarchy = isChecked();
+                    applyHierachyFilter();
+                };
+            };
+        filterHierarchyAction.setChecked( filterHierarchy );
+        hierarchyToolBarManager.add( filterHierarchyAction );
+        hierarchyToolBarManager.update( true );
+
+        hierarchyTreeViewer = new TreeViewer( hierarchySection, SWT.BORDER | SWT.VIRTUAL );
         Tree hierarchyTree = hierarchyTreeViewer.getTree();
+        hierarchySection.setClient( hierarchyTree );
         hierarchyTreeViewer.setUseHashlookup( true );
-        hierarchyTree.setLayoutData( new GridData( SWT.FILL, SWT.FILL, true, true, 1, 1 ) );
-
         hierarchyTreeViewer.setContentProvider( new InstallableUnitContentProvider( hierarchyTreeViewer )
         {
             @Override
@@ -104,56 +212,73 @@ public class DependencyHierarchyView
             }
         } );
 
-        ILabelProvider labelProvider = new InstallableUnitLabelProvider();
+        ILabelProvider labelProvider = new LabelProvider();
 
         hierarchyTreeViewer.setLabelProvider( labelProvider );
 
-        Composite listComposite = new Composite( sashForm, SWT.NONE );
-        listComposite.setLayout( new GridLayout( 1, false ) );
+        Section listSection = new Section( sashForm, Section.TITLE_BAR );
 
-        Label lblResolvedDependencies = new Label( listComposite, SWT.NONE );
-        lblResolvedDependencies.setText( "Resolved Dependencies" );
+        listSection.setText( "Resolved Dependencies" );
 
-        listTableViewer = new TableViewer( listComposite, SWT.BORDER | SWT.FULL_SELECTION | SWT.MULTI );
+        ToolBar listToolBar = new ToolBar( listSection, SWT.FLAT | SWT.RIGHT );
+        listSection.setTextClient( listToolBar );
+        ToolBarManager listToolbarManager = new ToolBarManager( listToolBar );
+        Action sortListAction =
+            new Action( "Sort", ResourceManager.getPluginImageDescriptor( "com.ifedorenko.p2browser", "icons/sort.gif" ) )
+            {
+                @Override
+                public int getStyle()
+                {
+                    return AS_CHECK_BOX;
+                }
+
+                @Override
+                public void run()
+                {
+                    sortList = isChecked();
+                    applyListFilter();
+                }
+            };
+        sortListAction.setChecked( sortList );
+        listToolbarManager.add( sortListAction );
+        Action filterListAction =
+            new Action( "Filter", ResourceManager.getPluginImageDescriptor( "com.ifedorenko.p2browser",
+                                                                            "icons/filter.gif" ) )
+            {
+                @Override
+                public int getStyle()
+                {
+                    return AS_CHECK_BOX;
+                }
+
+                @Override
+                public void run()
+                {
+                    filterList = isChecked();
+                    applyListFilter();
+                }
+            };
+        filterListAction.setChecked( filterList );
+        listToolbarManager.add( filterListAction );
+        listToolbarManager.update( true );
+
+        listTableViewer = new TableViewer( listSection, SWT.BORDER | SWT.FULL_SELECTION | SWT.MULTI );
+        Table listTable = listTableViewer.getTable();
+        listSection.setClient( listTable );
+
         listTableViewer.addSelectionChangedListener( new ISelectionChangedListener()
         {
             public void selectionChanged( SelectionChangedEvent event )
             {
-                IStructuredSelection selection = (IStructuredSelection) listTableViewer.getSelection();
+                Set<IInstallableUnit> units = toInstallableUnits( listTableViewer.getSelection() );
 
-                Set<IInstallableUnit> units = null;
-                if ( !selection.isEmpty() )
-                {
-                    units = new HashSet<IInstallableUnit>();
+                filterHierarchyAction.setEnabled( units == null );
+                filterHierarchyAction.setChecked( units == null ? filterHierarchy : false );
 
-                    Iterator<?> iter = selection.iterator();
-                    while ( iter.hasNext() )
-                    {
-                        Object element = iter.next();
-                        if ( element instanceof IInstallableUnit )
-                        {
-                            units.add( (IInstallableUnit) element );
-                        }
-                    }
-
-                    if ( units.isEmpty() )
-                    {
-                        units = null;
-                    }
-                }
-
-                InstallableUnitDAG filteredDag =
-                    units != null ? dag.filter( new InstallableUnitsMatcher( units ), true ) : dag;
-
-                // if ( !filteredDag.equals( dag ) )
-                {
-                    setDependencyTree( new InstallableUnitDependencyTree( filteredDag ) );
-                }
+                applyHierachyFilter( units );
             }
         } );
 
-        Table listTable = listTableViewer.getTable();
-        listTable.setLayoutData( new GridData( SWT.FILL, SWT.FILL, true, true, 1, 1 ) );
         listTableViewer.setContentProvider( new IStructuredContentProvider()
         {
             @Override
@@ -169,44 +294,68 @@ public class DependencyHierarchyView
             @Override
             public Object[] getElements( Object inputElement )
             {
-                if ( inputElement instanceof Collection )
+                if ( inputElement instanceof List )
                 {
-                    return ( (Collection<?>) inputElement ).toArray();
+                    List<?> elements = (List<?>) inputElement;
+
+                    if ( sortList )
+                    {
+                        elements = new ArrayList<Object>( elements ); // do not sort original collection
+                        Collections.sort( elements, new Comparator<Object>()
+                        {
+                            @Override
+                            public int compare( Object o1, Object o2 )
+                            {
+                                // unchecked cast is on purpose, so we known when element type changes ;-)
+                                IInstallableUnit u1 = (IInstallableUnit) o1;
+                                IInstallableUnit u2 = (IInstallableUnit) o2;
+
+                                return u1.getId().compareTo( u2.getId() );
+                            }
+                        } );
+                    }
+                    return elements.toArray();
                 }
                 return null;
             }
         } );
         listTableViewer.setLabelProvider( labelProvider );
 
+        listTableViewer.addFilter( new ViewerFilter()
+        {
+            @Override
+            public boolean select( Viewer viewer, Object parentElement, Object element )
+            {
+                if ( !filterList || unitMatcher == null )
+                {
+                    // no filter
+                    return true;
+                }
+
+                IInstallableUnit unit = null;
+                if ( element instanceof IInstallableUnit )
+                {
+                    unit = (IInstallableUnit) element;
+                }
+
+                if ( unit == null )
+                {
+                    return true;
+                }
+
+                return unitMatcher.match( unit );
+            }
+        } );
+
         sashForm.setWeights( new int[] { 1, 1 } );
 
-        createActions();
-        initializeToolBar();
-        initializeMenu();
-    }
-
-    /**
-     * Create the actions.
-     */
-    private void createActions()
-    {
-        // Create the actions
-    }
-
-    /**
-     * Initialize the toolbar.
-     */
-    private void initializeToolBar()
-    {
-        IToolBarManager toolbarManager = getViewSite().getActionBars().getToolBarManager();
-    }
-
-    /**
-     * Initialize the menu.
-     */
-    private void initializeMenu()
-    {
-        IMenuManager menuManager = getViewSite().getActionBars().getMenuManager();
+        Font font = listTableViewer.getTable().getFont();
+        FontData[] fontDatas = font.getFontData();
+        for ( FontData fontData : fontDatas )
+        {
+            fontData.setStyle( SWT.BOLD );
+        }
+        boldFont = new Font( getSite().getShell().getDisplay(), fontDatas );
     }
 
     @Override
@@ -283,5 +432,59 @@ public class DependencyHierarchyView
     private IInstallableUnit[] toArray( Collection<IInstallableUnit> collection )
     {
         return collection.toArray( new IInstallableUnit[collection.size()] );
+    }
+
+    void applyHierachyFilter()
+    {
+        applyHierachyFilter( toInstallableUnits( listTableViewer.getSelection() ) );
+    }
+
+    void applyHierachyFilter( Set<IInstallableUnit> units )
+    {
+        InstallableUnitDAG filteredDag = this.dag;
+
+        if ( units != null )
+        {
+            // apply right-pane selection filter
+            filteredDag = filteredDag.filter( new InstallableUnitsMatcher( units ), true );
+        }
+        else if ( unitMatcher != null && filterHierarchy )
+        {
+            filteredDag = filteredDag.filter( unitMatcher, true );
+        }
+
+        setDependencyTree( new InstallableUnitDependencyTree( filteredDag ) );
+    }
+
+    Set<IInstallableUnit> toInstallableUnits( ISelection selection )
+    {
+        Set<IInstallableUnit> units = null;
+        if ( selection instanceof IStructuredSelection && !selection.isEmpty() )
+        {
+            units = new HashSet<IInstallableUnit>();
+
+            Iterator<?> iter = ( (IStructuredSelection) selection ).iterator();
+            while ( iter.hasNext() )
+            {
+                Object element = iter.next();
+                if ( element instanceof IInstallableUnit )
+                {
+                    units.add( (IInstallableUnit) element );
+                }
+            }
+
+            if ( units.isEmpty() )
+            {
+                units = null;
+            }
+        }
+        return units;
+    }
+
+    void applyListFilter()
+    {
+        listTableViewer.getTable().setRedraw( false );
+        listTableViewer.refresh();
+        listTableViewer.getTable().setRedraw( true );
     }
 }
